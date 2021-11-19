815a717a1523c0b5d34ba20216c86751bb3073f3
Vegastrikes <74177293+Vegastrikes@users.noreply.github.com>
2021-11-19 08:19:24 +0300

    step2

--- Part1/course_information/src/App.js
+++ Part1/course_information/src/App.js
@@ -16,6 +16,15 @@ const App = () => {
   }
   const Content = (props) => {
     return (
+      <div>
+        <Part part={props.part1} exercise={props.exercise1}/>
+        <Part part={props.part2} exercise={props.exercise2}/>
+        <Part part={props.part3} exercise={props.exercise3}/>
+      </div>
+    )
+  }
+  const Part = (props) => {
+    return (
       <p>{props.part} {props.exercise}</p>
     )
   }
@@ -28,9 +37,10 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercise={exercises1}/>
-      <Content part={part2} exercise={exercises2}/>
-      <Content part={part3} exercise={exercises3}/>
+      <Content 
+      part1={part1} exercise1={exercises1} 
+      part2={part2} exercise2={exercises2} 
+      part3={part3} exercise3={exercises3}/>
       <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3}/>
     </div>
   )
